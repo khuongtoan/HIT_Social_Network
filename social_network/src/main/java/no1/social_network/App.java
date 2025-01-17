df9155ca42bc03b0f416c61fcd0841e7142eb57f
khuongtoan <kxtdumbo@gmail.com>
2025-01-17 18:58:00 +0700

    Inital commit

---
+++ social_network/src/main/java/no1/social_network/App.java
@@ -0,0 +1,13 @@
+package no1.social_network;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ social_network/src/test/java/no1/social_network/AppTest.java
@@ -0,0 +1,18 @@
+package no1.social_network;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true, "This test should always pass");
+	}
+}
